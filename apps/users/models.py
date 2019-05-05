36867f6c658a679230ef291b79b405217cfd3edd
ychcnm <1179139248@qq.com>
2019-05-05 17:05:44 +0800

    add all apps

--- apps/users/models.py
+++ apps/users/models.py
@@ -11,7 +11,7 @@ from django.db import models
 
 class UserProfile(AbstractUser):
     nick_name = models.CharField(max_length=50, verbose_name=u"昵称", default=u"")
-    birday = models.DateField(verbose_name=u"生日", null=True, blank=True)
+    birthday = models.DateField(verbose_name=u"生日", null=True, blank=True)
     gender = models.CharField(choices=(("male", u"男"), ("female", u"女")), default="female", max_length=6)
     address = models.CharField(max_length=100, verbose_name=u"地址", default=u"")
     mobile = models.CharField(max_length=11, null=True, blank=True)
